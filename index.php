b0ca29fb0cfbc3571766eb5af056e06535772d91
novakvova <novakvova@gmail.com>
2020-05-18 18:55:45 +0300

    Bagin php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include("style.php"); ?>
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+//$a=2;
+//$b="23";
+//$c=$a+$b;
+//echo "<h1>Hello php ".$c."</h1>";
+//echo "<h1>".$semen."</h1>";
+//if(isset($ivan)) {
+//    echo "<h1>" . $ivan . "</h1>";
+//}
+?>
+
+
+</body>
+</html>
---
+++ style.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+<?php
+$semen="Привіт хлопці";
+$ivan="Ivan";
+?>
