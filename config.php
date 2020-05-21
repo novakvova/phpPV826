1991c034251a811004a096ee2bd6276c59fd0c57
novakvova <novakvova@gmail.com>
2020-05-21 18:14:59 +0300

    Add conection azure

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
-define('DB_NAME','sopilkadb');
-define('DB_USER','peter');
-define('DB_PASSWORD','123456');
-define('DB_HOST','localhost');
+//define('DB_NAME','sopilkadb');
+//define('DB_USER','peter');
+//define('DB_PASSWORD','123456');
+//define('DB_HOST','localhost');
+
+define('DB_NAME','demodb');
+define('DB_USER','demouser');
+define('DB_PASSWORD','Qwe2-rty*66KddQwer-19mdjyf');
+define('DB_HOST','52.185.201.144');
